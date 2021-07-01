a363bbf79c6c1d2311ca17a482bccea1fb8536ac
Nicholas Geneva <ngeneva@nd.edu>
2021-07-01 14:25:31 -0400

    Removed Eval class names from data_util init

--- trphysx/data_utils/__init__.py
+++ trphysx/data_utils/__init__.py
@@ -1,7 +1,7 @@
 __all__ = ["dataset_cylinder", "dataset_lorenz", "dataset_grayscott", "dataset_phys"]
 
-from .data_utils import DataCollator, EvalDataCollator
-from .dataset_auto import AutoDataset, AutoPredictionDataset
+from .data_utils import DataCollator
+from .dataset_auto import AutoDataset
 from . import dataset_cylinder
 from . import dataset_lorenz
 from . import dataset_grayscott
